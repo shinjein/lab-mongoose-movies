0ca9190d96b125fc012fbff59658329d36988750
shinjein <shinjein@hu-berlin.de>
2021-04-20 16:22:52 +0100

    bonus completed

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -12,14 +12,19 @@ router.get('/celebrities', async (req, res) => {
 router.get('/celebrities/new', (req, res) => {
   res.render('celebs/new');
 });
-
+//edit celebrity
+router.get('/celebrities/:celebId/edit', async (req, res) => {
+  const celebId = req.params.celebId;
+  const celeb = await Celebrity.findById(celebId);
+  res.render('celebs/edit', { celeb });
+});
 
 //Delete Celebrity
 router.post('/celebrities/:celebId/delete', async (req, res) => {
   const celebId = req.params.celebId;
   await Celebrity.findByIdAndRemove(celebId);
   res.redirect('/celebrities');
-})
+});
 
 router.get('/celebrities/:celebId', async (req, res) => {
   const celebId = req.params.celebId;
@@ -33,5 +38,11 @@ router.post('/celebrities/new', async (req, res) => {
   res.redirect('/celebrities');
 });
 
+router.post('/celebrities/:celebId/edit', async (req, res) => {
+  const celebId = req.params.celebId;
+  const { name, occupation, catchPhrase } = req.body;
+  await Celebrity.findByIdAndUpdate(celebId, { occupation, catchPhrase });
+  res.redirect('/celebrities')
+});
 
 module.exports = router;
